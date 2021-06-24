df196eaae0cf871f69b3f701f11d70ea7a956f25
Sergey87 <serge.techno@gmail.com>
2021-06-24 23:01:44 +0300

    home task done

--- js/main.js
+++ js/main.js
@@ -723,3 +723,60 @@ const hotels3 = [
       console.log(country, citiesResult);
     }
   }
+
+// ЗАДАЧА 
+// Напишите функция deepEqual, которая сможет сравнивать 2 объекта с разными уровнями вложенности.Напимер:
+
+const obj1 = {
+  a: 'a',
+  b: {
+    a: 'a',
+    b: 'b',
+    c: {
+      a: 1,
+    },
+  },
+};
+const obj2 = {
+  b: {
+    c: {
+      a: 1,
+    },
+    b: 'b',
+    a: 'a',
+  },
+  a: 'a',
+};
+const obj3 = {
+  a: {
+    c: {
+      a: 'a',
+    },
+    b: 'b',
+    a: 'a',
+  },
+  b: 'b',
+};
+
+function deepEqual(a, b) {
+  if (a === b) return true;
+
+  if (a === null || typeof (a) !== "object" || b === null || typeof (b) !== "object") return false;
+
+  let propertiesInA = 0, propertiesInB = 0;
+  for (let property in a) {
+    propertiesInA = propertiesInA + 1;
+  }
+  for (let property in b) {
+    propertiesInB = propertiesInB + 1;
+    if (!(property in a) || !deepEqual(a[property], b[property])) {
+      return false;
+    }
+  }
+  return propertiesInA === propertiesInB;
+}
+
+deepEqual(obj1, obj2); 
+console.log(deepEqual(obj1, obj2)); // true
+deepEqual(obj1, obj3); 
+console.log(deepEqual(obj1, obj3)); // false
